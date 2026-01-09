1a9726d9a412c52b91ffc7d0a12e35cf985fbfd3
Sebastian <sebastian.karl08@htl-salzburg.ac.at>
2026-01-09 09:09:03 +0100

    Error Fixing

--- Assets/MyGame/Scripts/RoomLogic.cs
+++ Assets/MyGame/Scripts/RoomLogic.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using TMPro;
 
 public class RoomLogic : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Status")]
+    public float currentTemp = 24f;
+    public bool isCandleLit = false;
 
-    // Update is called once per frame
+    [Header("Balancing (wird vom Schwierigkeitsgrad gesteuert)")]
+    public float tempDropRate = 0.5f;
+    public float tempHeatRate = 1.0f;
+
+    [Header("Referenzen")]
+    public TextMeshProUGUI tempDisplay;
+    public GameObject candleVisual;
+
     void Update()
     {
-        
+        if (isCandleLit)
+        {
+            currentTemp += (tempHeatRate - tempDropRate) * Time.deltaTime;
+        }
+        else
+        {
+            currentTemp -= tempDropRate * Time.deltaTime;
+        }
+
+        currentTemp = Mathf.Clamp(currentTemp, 0, 40);
+        if(tempDisplay != null) 
+            tempDisplay.text = currentTemp.ToString("F1") + "°C";
+
+        if (currentTemp <= 0)
+        {
+            Debug.Log("Katze erfroren! Spiel vorbei.");
+        }
+    }
+
+    private void OnMouseDown() 
+    {
+        CandleManager.Instance.TryToggleCandle(this);
+    }
+
+    public void SetCandleState(bool state)
+    {
+        isCandleLit = state;
+        if(candleVisual != null) candleVisual.SetActive(state);
     }
 }
